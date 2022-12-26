68a87daf3ed20fb5d61c06181390a18f6603616c
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-12-26 17:11:59 +0100

    added toggles for enabling player collisions

--- src/DynamicsHandler.py
+++ src/DynamicsHandler.py
@@ -16,6 +16,8 @@ class DynamicsHandler:
     self.stopped = True
     self.prevTime = time.time()
     self.lock = Lock()
+    self.doPlayer1Collide = True
+    self.doPlayer2Collide = True
   
   def start(self):
     self.stopped = False
@@ -25,6 +27,10 @@ class DynamicsHandler:
 
   def stop(self):
     self.stopped = True
+
+  def enableCollisions(self, player1=True, player2=True):
+    self.doPlayer1Collide = player1
+    self.doPlayer2Collide = player2
 
   def updatePlayerCoords(self, coordsPlayer1, coordsPlayer2):
     self.prevCoordsPlayer1 = self.coordsPlayer1
@@ -57,30 +63,32 @@ class DynamicsHandler:
       if newPuckCoords[1] - puckSize < self.borderY or newPuckCoords[1] + puckSize > self.fieldShape[0] - self.borderY:
         puckAngle = -puckAngle
 
-      # Calculate the new Puck Angle on bounce with player 1 position and angle, considering the puck size
-      # TODO: flip first and second point
-      player1StartCoord = self.coordsPlayer1[0]
-      x1 = int(player1StartCoord[0] + self.coordsPlayer2[2] * np.cos(self.coordsPlayer1[1]))
-      y1 = int(player1StartCoord[1] + self.coordsPlayer2[2] * np.sin(self.coordsPlayer1[1]))
-      player1EndCoord = [x1, y1]
-      player1Angle = self.coordsPlayer1[2]
-      player1NormalAngle = player1Angle + np.pi/2
-      if puckCoords[0] - puckSize < player1EndCoord[0] and puckCoords[0] + puckSize > player1StartCoord[0] and puckCoords[1] - puckSize < player1EndCoord[1] and puckCoords[1] + puckSize > player1StartCoord[1]:
-        # Calculate the new Puck Angle
-        puckAngle = -puckAngle + 2 * player1NormalAngle
-        # TODO: Calculate the new Puck Speed
+      if self.doPlayer1Collide:
+        # Calculate the new Puck Angle on bounce with player 1 position and angle, considering the puck size
+        # TODO: Solve collision issue
+        player1StartCoord = self.coordsPlayer1[0]
+        x1 = int(player1StartCoord[0] - self.coordsPlayer2[2] * np.cos(self.coordsPlayer1[1]))
+        y1 = int(player1StartCoord[1] - self.coordsPlayer2[2] * np.sin(self.coordsPlayer1[1]))
+        player1EndCoord = [x1, y1]
+        player1Angle = self.coordsPlayer1[2]
+        player1NormalAngle = player1Angle + np.pi/2
+        if puckCoords[0] - puckSize < player1EndCoord[0] and puckCoords[0] + puckSize > player1StartCoord[0] and puckCoords[1] - puckSize < player1EndCoord[1] and puckCoords[1] + puckSize > player1StartCoord[1]:
+          # Calculate the new Puck Angle
+          puckAngle = -puckAngle + 2 * player1NormalAngle
+          # TODO: Calculate the new Puck Speed
 
-      # Calculate the new Puck Angle on bounce with player 2 position and angle, considering the puck size
-      player2StartCoord = self.coordsPlayer2[0]
-      x2 = int(player2StartCoord[0] + self.coordsPlayer2[2] * np.cos(self.coordsPlayer2[1]))
-      y2 = int(player2StartCoord[1] + self.coordsPlayer2[2] * np.sin(self.coordsPlayer2[1]))
-      player2EndCoord = [x2, y2]
-      player2Angle = self.coordsPlayer2[2]
-      player2NormalAngle = player2Angle + np.pi/2
-      if puckCoords[0] - puckSize < player2EndCoord[0] and puckCoords[0] + puckSize > player2StartCoord[0] and puckCoords[1] - puckSize < player2EndCoord[1] and puckCoords[1] + puckSize > player2StartCoord[1]:
-        # Calculate the new Puck Angle
-        puckAngle = -puckAngle + 2 * player2NormalAngle
-        # TODO: Calculate the new Puck Speed
+      if self.doPlayer2Collide:
+        # Calculate the new Puck Angle on bounce with player 2 position and angle, considering the puck size
+        player2StartCoord = self.coordsPlayer2[0]
+        x2 = int(player2StartCoord[0] + self.coordsPlayer2[2] * np.cos(self.coordsPlayer2[1]))
+        y2 = int(player2StartCoord[1] + self.coordsPlayer2[2] * np.sin(self.coordsPlayer2[1]))
+        player2EndCoord = [x2, y2]
+        player2Angle = self.coordsPlayer2[2]
+        player2NormalAngle = player2Angle + np.pi/2
+        if puckCoords[0] - puckSize < player2EndCoord[0] and puckCoords[0] + puckSize > player2StartCoord[0] and puckCoords[1] - puckSize < player2EndCoord[1] and puckCoords[1] + puckSize > player2StartCoord[1]:
+          # Calculate the new Puck Angle
+          puckAngle = -puckAngle + 2 * player2NormalAngle
+          # TODO: Calculate the new Puck Speed
 
       # Update the puck coords, speed and angle
       with self.lock:
