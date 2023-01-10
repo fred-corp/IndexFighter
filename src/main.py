7eca94b09548a623064730b03c082bbd4b6839bb
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-01-10 15:41:43 +0100

    minor changes to main.py

--- src/main.py
+++ src/main.py
@@ -8,13 +8,14 @@ dynLock = Lock()
 
 
 
-def startGame(playerNum = 1):
-
+def startGame(debug=0):
+  _debug = debug == 1
   borderX = 80
   borderY = 50
   friction = 1
   fieldShape = (720, 1280)
-  coordsPuck = [[640, 360], 20, 250, np.pi/4]
+  #coordsPuck = [[640, 360], 20, 250, np.pi/4]
+  coordsPuck = [[640, 360], 20, 100, 0]
 
   camHandler = CameraHandler(fieldShape)
   camHandler.start()
@@ -22,14 +23,17 @@ def startGame(playerNum = 1):
 
   coordsPlayer1, coordsPlayer2 = camHandler.getCoords()
   dynamicsHandler = DynamicsHandler(fieldShape, friction, borderX, borderY, coordsPlayer1, coordsPlayer2, coordsPuck)
+  # Debug
+  # dynamicsHandler.enableCollisions(player1=True, player2=False)
   dynamicsHandler.start()
   
   while True:
     coordsPlayer1, coordsPlayer2 = camHandler.getCoords()
-    playfield = playField.getField(coordsPlayer1, coordsPlayer2, coordsPuck, drawPuck=False)
+    playfield = playField.getField(coordsPlayer1, coordsPlayer2, coordsPuck)
     dynamicsHandler.updatePlayerCoords(coordsPlayer1, coordsPlayer2)
     coordsPuck = dynamicsHandler.getPuckCoords()
-    cv2.imshow("Image", camHandler.getImage())
+    if _debug:
+      cv2.imshow("Image", camHandler.getImage())
     cv2.imshow("Playfield", playfield)
     ##time.sleep(0.001)
     if cv2.waitKey(1) == 27:
@@ -41,6 +45,5 @@ def startGame(playerNum = 1):
 
 
 if __name__ == '__main__':
-
-  startGame(sys.argv[1] if len(sys.argv) > 1 else 1)
+  startGame(sys.argv[1] if len(sys.argv) > 1 else 0)
   print("Done")
