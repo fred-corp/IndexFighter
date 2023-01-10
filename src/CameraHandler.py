d28eb431d944d37f03d8f31c993ce5034bb846f3
fred-corp <7109145+fred-corp@users.noreply.github.com>
2023-01-10 15:41:21 +0100

    moved logic out of mediapipe loop

--- src/CameraHandler.py
+++ src/CameraHandler.py
@@ -135,17 +135,17 @@ class CameraHandler:
               P2angle += np.pi
             coordsPlayer2 = [[x1p2, y1p2], P2angle+np.pi, coordsPlayer2[2]]
 
-            cTime = time.time()
-            fps = 1 / (cTime - pTime)
-            pTime = cTime
+          cTime = time.time()
+          fps = 1 / (cTime - pTime)
+          pTime = cTime
 
-            cv2.putText(image, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                        (255, 0, 255), 3)
+          cv2.putText(image, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                      (255, 0, 255), 3)
 
-            with self.camLock:
-              self.coordsPlayer1 = coordsPlayer1
-              self.coordsPlayer2 = coordsPlayer2
-              self.image = image
+          with self.camLock:
+            self.coordsPlayer1 = coordsPlayer1
+            self.coordsPlayer2 = coordsPlayer2
+            self.image = image
 
 
   def stop(self):
